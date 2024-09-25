cfe78cb3598cdcc960495a2933f444d653289cff
IliaGurelev <vailade.you@bk.ru>
2024-09-25 12:32:45 +0300

    First commit

---
+++ src/components/icons/MessagerIcon.vue
@@ -0,0 +1,177 @@
+<template>
+  <svg
+    id="Layer_1"
+    style="enable-background: new 0 0 74 74"
+    version="1.1"
+    viewBox="0 0 74 74"
+    xml:space="preserve"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <g>
+      <path
+        class="st10"
+        d="M24.9,6.9C11.7,6.9,1,15,1,25c0,5.4,3.1,10.3,8.1,13.6v10.9l10.8-6.7c1.6,0.3,3.3,0.4,5,0.4   C38,43.2,48.7,35,48.7,25C48.7,15,38,6.9,24.9,6.9z"
+      />
+      <path
+        class="st4"
+        d="M49.1,24.5c13.2,0,23.9,8.1,23.9,18.1c0,5.4-3.1,10.3-8.1,13.6v10.9l-10.8-6.7c-1.6,0.3-3.3,0.4-5,0.4   c-13.2,0-23.9-8.1-23.9-18.1C25.3,32.6,36,24.5,49.1,24.5z"
+      />
+      <circle class="st6" cx="62" cy="42.7" r="2.9" />
+      <circle class="st6" cx="49.1" cy="42.7" r="2.9" />
+      <circle class="st6" cx="36.3" cy="42.7" r="2.9" />
+    </g>
+  </svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.st0 {
+  fill: #7688a1;
+}
+.st1 {
+  fill: #fabb8e;
+}
+.st2 {
+  fill: #e69d6a;
+}
+.st3 {
+  fill: #594667;
+}
+.st4 {
+  fill: #e73a36;
+}
+.st5 {
+  fill: #cfe5e4;
+}
+.st6 {
+  fill: #ffffff;
+}
+.st7 {
+  fill: #eef1f6;
+}
+.st8 {
+  fill: #8edafa;
+}
+.st9 {
+  fill: #fff383;
+}
+.st10 {
+  fill: #70b7d6;
+}
+.st11 {
+  fill: #ffaec3;
+}
+.st12 {
+  fill: #ff9db2;
+}
+.st13 {
+  fill: #f18da2;
+}
+.st14 {
+  fill: #ffdf97;
+}
+.st15 {
+  fill: #49375b;
+}
+.st16 {
+  fill: #452e1b;
+}
+.st17 {
+  fill: #fef6b0;
+}
+.st18 {
+  fill: #ffcca9;
+}
+.st19 {
+  fill: #fed766;
+}
+.st20 {
+  fill: #8a9db2;
+}
+.st21 {
+  fill: #5d738c;
+}
+.st22 {
+  fill: #2c3e50;
+}
+.st23 {
+  fill: #ec685e;
+}
+.st24 {
+  fill: #54758c;
+}
+.st25 {
+  fill: #d12828;
+}
+.st26 {
+  fill: #69567c;
+}
+.st27 {
+  fill: #857399;
+}
+.st28 {
+  fill: #705d82;
+}
+.st29 {
+  fill: #eaa678;
+}
+.st30 {
+  fill: #d88d5b;
+}
+.st31 {
+  fill: #3597d3;
+}
+.st32 {
+  fill: #e27491;
+}
+.st33 {
+  fill: #e4f2f1;
+}
+.st34 {
+  fill: #ffe576;
+}
+.st35 {
+  fill: #ffb8cb;
+}
+.st36 {
+  fill: #c91f1f;
+}
+.st37 {
+  fill: #e0e2ee;
+}
+.st38 {
+  fill: #fefefe;
+}
+.st39 {
+  fill: #e5e5e5;
+}
+.st40 {
+  fill: #d3d3d3;
+}
+.st41 {
+  fill: #c2c3c4;
+}
+.st42 {
+  fill: none;
+}
+.st43 {
+  fill: #fffbd7;
+}
+.st44 {
+  fill: #cccccc;
+}
+.st45 {
+  fill: #fff5b0;
+}
+.st46 {
+  fill: #edeff4;
+}
+.st47 {
+  fill: #f0f2f7;
+}
+.st48 {
+  fill: #d3ebf7;
+}
+</style>
